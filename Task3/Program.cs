0a8fd49e2fb55bfea0d5563484e0999886dedb92
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-10 23:41:36 +0300

    задача 3 готова

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,3 +4,12 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.Write("номер дня ");
+int day = Convert.ToInt32(Console.ReadLine());
+string[] dayOfweek = {"пн", "вт", "ср", "чт", "пт", "сб", "вс"};
+Console.WriteLine(day % 7);
+if (day % 7 >= 5)
+Console.WriteLine(true);
+else
+Console.WriteLine(false);
