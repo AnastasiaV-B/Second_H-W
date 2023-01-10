378db8bd2057c09e070614f680a41fd7f4bf839b
garnova_n@mail.ru <garnova_n@mail.ru>
2023-01-11 00:55:02 +0300

    задача 2 готова

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -3,3 +3,15 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 
+Console.Write("Введите число ");
+int n = Convert.ToInt32(Console.ReadLine());
+if(n > 99)
+{
+    Console.WriteLine((n % 10));
+}
+else
+{
+if(n > 99) 
+   n = n / 10;
+Console.Write("третьей цифры нет");
+}
